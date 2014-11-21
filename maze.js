842d7cce2b4f5262762d635a43e73f316740d75a
JhovanDaniel <jd069@hotmail.com>
2014-11-21 13:19:17 -0400

    Added js and html

---
+++ maze.js
@@ -0,0 +1,12 @@
+
+window.onload = function() {
+document.getElementById("boundary1").onmouseover = changeColor;  
+};
+
+
+
+function changeColor() {
+document.getElementById("boundary1").style.backgroundColor = "#ff8888";
+clickMe.style.color = "red";
+}
+
